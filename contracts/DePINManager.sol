f48df2d4739ad09d44145feb077434e524272292
Elvin3x <elvin3block@gmail.com>
2024-10-22 16:14:44 +0300

    v.0.1.2

--- contracts/DePINManager.sol
+++ contracts/DePINManager.sol
@@ -2,11 +2,10 @@
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 
-contract DePINManager is Ownable, Pausable, ReentrancyGuard {
+contract DePINManager is Ownable, ReentrancyGuard {
     using EnumerableSet for EnumerableSet.AddressSet;
     using EnumerableSet for EnumerableSet.UintSet;
 
@@ -50,11 +49,14 @@ contract DePINManager is Ownable, Pausable, ReentrancyGuard {
         _;
     }
 
-    constructor() {
+    constructor(address initialOwner) Ownable(initialOwner) {
         _assetCounter = 0;
     }
 
-    function createAsset(string memory name, string memory metadataURI) external whenNotPaused returns (uint256) {
+    function createAsset(string memory name, string memory metadataURI) external returns (uint256) {
+        require(bytes(name).length > 0, "Name cannot be empty");
+        require(bytes(metadataURI).length > 0, "MetadataURI cannot be empty");
+
         _assetCounter += 1;
         uint256 newAssetId = _assetCounter;
 
@@ -77,7 +79,7 @@ contract DePINManager is Ownable, Pausable, ReentrancyGuard {
         return newAssetId;
     }
 
-    function updateAsset(uint256 assetId, string memory metadataURI) external whenNotPaused assetExists(assetId) onlyAssetOwner(assetId) {
+    function updateAsset(uint256 assetId, string memory metadataURI) external assetExists(assetId) onlyAssetOwner(assetId) {
         Asset storage asset = _assets[assetId];
         asset.metadataURI = metadataURI;
         asset.updatedAt = block.timestamp;
@@ -85,7 +87,7 @@ contract DePINManager is Ownable, Pausable, ReentrancyGuard {
         emit AssetUpdated(assetId, metadataURI, msg.sender);
     }
 
-    function transferAsset(uint256 assetId, address to) external whenNotPaused assetExists(assetId) nonReentrant {
+    function transferAsset(uint256 assetId, address to) external assetExists(assetId) nonReentrant {
         if (to == address(0)) revert InvalidRecipientAddress();
         address owner = _assets[assetId].owner;
         if (msg.sender != owner && getApproved(assetId) != msg.sender) revert CallerNotOwnerNorApproved();
@@ -108,7 +110,7 @@ contract DePINManager is Ownable, Pausable, ReentrancyGuard {
         emit AssetTransferred(assetId, from, to);
     }
 
-    function approve(address to, uint256 assetId) external whenNotPaused assetExists(assetId) onlyAssetOwner(assetId) {
+    function approve(address to, uint256 assetId) external assetExists(assetId) onlyAssetOwner(assetId) {
         _assetApprovals[assetId] = to;
         emit Approval(msg.sender, to, assetId);
     }
@@ -117,7 +119,7 @@ contract DePINManager is Ownable, Pausable, ReentrancyGuard {
         return _assetApprovals[assetId];
     }
 
-    function deactivateAsset(uint256 assetId) external whenNotPaused assetExists(assetId) onlyAssetOwner(assetId) {
+    function deactivateAsset(uint256 assetId) external assetExists(assetId) onlyAssetOwner(assetId) {
         Asset storage asset = _assets[assetId];
         if (!asset.active) revert AssetAlreadyDeactivated();
         asset.active = false;
@@ -125,7 +127,7 @@ contract DePINManager is Ownable, Pausable, ReentrancyGuard {
         emit AssetDeactivated(assetId, msg.sender);
     }
 
-    function reactivateAsset(uint256 assetId) external whenNotPaused assetExists(assetId) onlyAssetOwner(assetId) {
+    function reactivateAsset(uint256 assetId) external assetExists(assetId) onlyAssetOwner(assetId) {
         Asset storage asset = _assets[assetId];
         if (asset.active) revert AssetAlreadyActive();
         asset.active = true;
@@ -153,13 +155,5 @@ contract DePINManager is Ownable, Pausable, ReentrancyGuard {
             owners[i] = _assetOwners.at(i);
         }
         return owners;
-    }
-
-    function pause() external onlyOwner {
-        _pause();
-    }
-
-    function unpause() external onlyOwner {
-        _unpause();
     }
 }
