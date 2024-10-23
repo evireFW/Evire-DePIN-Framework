53232085ccefb8d567f378dd274d56b4b0b9e945
Elvin3x <elvin3block@gmail.com>
2024-10-23 15:16:53 +0300

    pragma update

--- contracts/OracleIntegration.sol
+++ contracts/OracleIntegration.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0; // You could update to a more recent version if desired
+pragma solidity ^0.8.26;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
